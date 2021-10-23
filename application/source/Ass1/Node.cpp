9500ec1521fa51164146bf7096ab51544460a96c
Jacob Lammert <son.of.archilles@gmail.com>
2021-10-23 16:50:53 +0200

    Added Node class

--- application/source/Ass1/Node.cpp
+++ application/source/Ass1/Node.cpp
@@ -3,3 +3,78 @@
 //
 
 #include "Node.h"
+#include <string>
+
+Node::Node() {
+
+}
+
+Node *Node::getParent() {
+    return parent;
+}
+
+Node *Node::getChildren(std::string name) {
+
+    for (int i = 0; i < children.size(); ++i){
+        if (children[i]->getName() == name){
+            return children[i];
+        }
+    }
+
+    return nullptr;
+}
+
+Node *Node::removeChildren(std::string name) {
+
+    Node* child = nullptr;
+
+    for (int i = 0; i < children.size(); ++i){
+        if (children[i]->getName() == name){
+            child = children[i];
+            children.erase(children.begin() + i,children.begin() + i + 1);
+            return child;
+        }
+    }
+
+    return child;
+}
+
+std::vector<Node *> Node::getChildrenList() {
+    return children;
+}
+
+void Node::setParent(Node *parenta) {
+    this->parent = parenta;
+}
+
+void Node::setLocalTransform(glm::fmat4 localtransform) {
+    this->localTransform = localtransform;
+}
+
+void Node::setWorldTransform(glm::fmat4 worldtransform) {
+    worldTransform = worldtransform;
+}
+
+void Node::addChildren(Node *child) {
+    children.push_back(child);
+}
+
+glm::fmat4 Node::getLocalTransform() {
+    return localTransform;
+}
+
+glm::fmat4 Node::getWorldTransform() {
+    return worldTransform;
+}
+
+std::string Node::getName() {
+    return name;
+}
+
+std::string Node::getPath() {
+    return path;
+}
+
+int Node::getDepth() {
+    return depth;
+}
--- application/source/Ass1/Node.h
+++ application/source/Ass1/Node.h
@@ -5,8 +5,46 @@
 #ifndef OPENGLFRAMEWORK_NODE_H
 #define OPENGLFRAMEWORK_NODE_H
 
+#include <string>
+#include <vector>
+#include <glm/detail/type_mat4x4.hpp>
+
 
 class Node {
+
+public:
+    Node();
+
+    Node* getParent();
+    Node* getChildren(std::string name);
+    Node* removeChildren(std::string name);
+
+    std::vector<Node*> getChildrenList();
+
+    void setParent(Node* parent);
+    void setLocalTransform(glm::fmat4 localtransform);
+    void setWorldTransform(glm::fmat4 worldtransform);
+    void addChildren(Node* child);
+
+    glm::fmat4 getLocalTransform();
+    glm::fmat4 getWorldTransform();
+
+    std::string getName();
+    std::string getPath();
+
+    int getDepth();
+
+
+
+private:
+    Node *parent;
+    std::vector<Node*> children;
+    std::string name;
+    std::string path;
+    int depth;
+    glm::fmat4 localTransform;
+    glm::fmat4 worldTransform;
+
 
 };
 
